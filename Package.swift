44b994ed1daaceb3955e7ec0c7094eaf0fe63d32
Wenshun Bai <wenshun@clearbridgemobile.com>
2024-02-15 12:54:57 -0500

    fix supported platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "EaseLive",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v12),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
